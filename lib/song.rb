47a8925355357aee922ddde0baba37bf5d82f384
Michael Blandford <mdblandford@gmail.com>
2018-04-30 13:12:09 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song 
+  attr_accessor :name, :artist 
+  
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+  end
+  
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+end
