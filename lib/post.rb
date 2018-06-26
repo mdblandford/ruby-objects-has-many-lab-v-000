32fee9a959010b3c00b74dcdee935960a81d638a
Michael Blandford <mdblandford@gmail.com>
2018-06-26 15:39:51 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,11 @@
-class Post 
-  attr_accessor :title, :author 
-  
-  def initialize(title, author=nil)
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title, author= nil)
     @title = title
     @author = author
   end
-  
+
   def author_name
     self.author ? self.author.name : nil
   end
