8898d7890bd2817675f28753c70b6cc215975cc2
Michael Blandford <mdblandford@gmail.com>
2018-06-26 15:37:34 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,14 +7,10 @@ class Author
     @posts = []
   end
 
-  #def posts
-  #  @posts
-  #end
-
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count += 1
+    #@@post_count += 1
   end
 
   def add_post_by_title(title)
